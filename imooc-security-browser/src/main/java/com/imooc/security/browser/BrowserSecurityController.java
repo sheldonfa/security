e52e793c096a5fc58c4b7bd18c36d5f7ab32f90c
fangxin <xfang@ling-ban.com>
2017-12-17 23:13:25 +0800

    我的测试分支

--- imooc-security-browser/src/main/java/com/imooc/security/browser/BrowserSecurityController.java
+++ imooc-security-browser/src/main/java/com/imooc/security/browser/BrowserSecurityController.java
@@ -3,6 +3,8 @@ package com.imooc.security.browser;
 import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
 import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
 import org.springframework.security.web.savedrequest.RequestCache;
 import org.springframework.security.web.savedrequest.SavedRequest;
@@ -19,6 +21,8 @@ public class BrowserSecurityController {
 
     private RequestCache requestCache = new HttpSessionRequestCache();
 
+    private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+
 
     @RequestMapping("/authentication/require")
     public String requireAuthentication(HttpServletRequest request, HttpServletResponse response) {
@@ -28,9 +32,9 @@ public class BrowserSecurityController {
             String targetUrl = savedRequest.getRedirectUrl();
             logger.info("引发跳转的请求是：" + targetUrl);
             if(StringUtils.endsWithIgnoreCase(targetUrl,".html")){
-
+                redirectStrategy.sendRedirect(request, response, "myloginpage.html");
             }
         }
-        return null;
+        return new ;
     }
 }
